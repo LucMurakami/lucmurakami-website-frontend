9412e725a5618fedca527b5ece9d18e7e6ffa202
Luc Murakami <luc.murakami@hotmail.com>
2020-06-16 14:02:25 -0700

    added motivationalmicroservicescreen feature

---
+++ src/Views/MotivationMicroservice/MotivationMicroserviceScreen.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  Container,
+  makeStyles,
+  Paper,
+  Typography,
+  Button,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  appBarSpacer: theme.mixins.toolbar,
+  root: {
+    paddingBottom: 20,
+    minWidth: 500,
+    margin: 20,
+  },
+}));
+
+const MotivationMicroserviceScreen = ({ data }) => {
+  const classes = useStyles();
+  return (
+    <Container>
+      <div className={classes.appBarSpacer} />
+      <br />
+      <Typography variant="h2">Daily Motivational Image!</Typography>
+      <Paper className={classes.root}>
+        <Grid container direction="column" justify="center" alignItems="center" spacing={2}>
+          <Grid item>
+            <Typography gutterBottom variant="h5" component="h2">
+              {data && data.title}
+              {!data && "Loading..."}
+            </Typography>
+          </Grid>
+          <Grid item>{data && <img src={data.image} alt="Motivation" width="480"/>}</Grid>
+          <Grid item>
+            <Typography variant="body2" color="textSecondary" component="p">
+              Number of Upvotes: {data && data.upvotes}
+              {!data && "Calculating..."}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => {
+                if (data) {
+                  window.open(`https://www.reddit.com/${data.permalink}`)
+                }
+              }}
+            >
+              Go to Reddit
+            </Button>
+          </Grid>
+          <Grid item>
+            <Divider />
+            <br />
+            <Typography variant="body2" color="textSecondary" components="p" align="center">
+              All information taken from highest upvoted post on r/getMotivated, a subreddit of reddit.com.{<br />}
+              Idea credit to Enoch. All rights to image and title belong to owner of the original reddit post.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Container>
+  );
+};
+
+export default MotivationMicroserviceScreen;
