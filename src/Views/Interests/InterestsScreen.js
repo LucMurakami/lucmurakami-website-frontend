0d9b43754c4523bcdeee4faedeabbb4268b9bf44
Luc Murakami <luc.murakami@hotmail.com>
2020-06-15 18:36:14 -0700

    Updated broken link for interests page

--- src/Views/Interests/InterestsScreen.js
+++ src/Views/Interests/InterestsScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, makeStyles, Typography, Paper } from "@material-ui/core";
+import { Container, makeStyles, Typography } from "@material-ui/core";
 
 import ControlledExpansionPanel from "../../Components/ControlledExpansionPanel";
 
